87b9c01e082390de0a89ec7c9f50b312f45c9c17
Arguish <jhergon8@gmail.com>
2023-02-27 10:09:28 +0000

    Rutas archivos

--- App/Phaser/PhaserMain.js
+++ App/Phaser/PhaserMain.js
@@ -1,12 +1,14 @@
 import Phaser from "./lib/phaser.js";
 import Game from "./_Scenes/game.js";
 
-console.dir(Phaser);
+//console.dir(Phaser);
 
-export default new Phaser.Game({
+const gameConfig = {
   type: Phaser.AUTO,
   parent: "gameDiv",
   width: 480,
   height: 640,
   scene: Game,
-});
+};
+
+export default new Phaser.Game(gameConfig);
--- App/Phaser/_Scenes/game.js
+++ App/Phaser/_Scenes/game.js
@@ -5,7 +5,11 @@ export default class Game extends Phaser.Scene {
     super("game"); //Every Scene has to define a unique key. We do that on line 7 in the constructor by calling super('game').
   }
 
-  preload() {}
+  preload() {
+    this.load.image("background", "../Phaser/Assets/Test/bg_layer1.png");
+  }
 
-  create() {}
+  create() {
+    this.add.image(240, 320, "background");
+  }
 }
