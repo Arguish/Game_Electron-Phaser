d7bd38ddf131255b40c8922d519105b20f13a2f0
Arguish <jhergon8@gmail.com>
2023-02-28 10:56:10 +0000

    Phaser autojump

--- App/Phaser/_Scenes/game.js
+++ App/Phaser/_Scenes/game.js
@@ -1,6 +1,8 @@
 import Phaser from "../lib/phaser.js";
 
 export default class Game extends Phaser.Scene {
+  /** @type {Phaser.Physics.Arcade.Sprite} */
+  player;
   constructor() {
     super("game"); //Every Scene has to define a unique key. We do that on line 7 in the constructor by calling super('game').
   }
@@ -8,10 +10,41 @@ export default class Game extends Phaser.Scene {
   preload() {
     this.load.image("background", "../Phaser/Assets/Test/bg_layer1.png");
     this.load.image("platform", "../Phaser/Assets/Test/ground_grass.png");
+    this.load.image("bunny-stand", "../Phaser/Assets/Test/bunny2_stand.png");
   }
 
   create() {
     this.add.image(240, 320, "background");
-    this.physics.add.image(240, 320, "platform").setScale(0.5);
+
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
+
+      /** @type {Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body;
+      body.updateFromGameObject();
+    }
+    this.player = this.physics.add
+      .sprite(240, 320, "bunny-stand")
+      .setScale(0.5);
+
+    this.physics.add.collider(platforms, this.player);
+  }
+
+  update() {
+    // find out from Arcade Physics if the player's physics body
+    // is touching something below it
+    const touchingDown = this.player.body.touching.down;
+
+    if (touchingDown) {
+      // this makes the bunny jump straight up
+      this.player.setVelocityY(-300);
+    }
   }
 }
